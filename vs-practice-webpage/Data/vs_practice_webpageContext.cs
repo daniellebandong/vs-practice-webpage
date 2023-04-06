ab49fbe68eef28b96e16913a2ad72a6765f27e47
daniellebandong <46451872+daniellebandong@users.noreply.github.com>
2023-04-06 13:18:26 -0600

    Add project files.

---
+++ vs-practice-webpage/Data/vs_practice_webpageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using vs_practice_webpage.Models;
+
+namespace vs_practice_webpage.Data
+{
+    public class vs_practice_webpageContext : DbContext
+    {
+        public vs_practice_webpageContext (DbContextOptions<vs_practice_webpageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<vs_practice_webpage.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ vs-practice-webpage/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using vs_practice_webpage.Data;
+using vs_practice_webpage.Models;
+
+namespace vs_practice_webpage.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly vs_practice_webpage.Data.vs_practice_webpageContext _context;
+
+        public IndexModel(vs_practice_webpage.Data.vs_practice_webpageContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
